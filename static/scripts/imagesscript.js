685e6eb59b97666605bb0c0b9331256ec0c14a91
Rigers Collaku <rigers.collaku@icloud.com>
2023-11-01 11:52:58 +0100

    changes from carousel to gallery

--- static/scripts/imagesscript.js
+++ static/scripts/imagesscript.js
@@ -1,16 +1,40 @@
-function openImageWindow(imageUrl) {
-    const width = 800; // Set the desired width of the new window
-    const height = 600; // Set the desired height of the new window
+// gallery-navigation.js
 
-    // Calculate the left and top positions for centering the new window
-    const left = (window.innerWidth - width) / 2;
-    const top = (window.innerHeight - height) / 2;
+let imageUrls = []; // Array to store image URLs
+let currentIndex = 0; // Index of the currently displayed image
+let newWindow = null; // Reference to the new window
 
-    // Open a new window with the specified dimensions and URL
-    const newWindow = window.open(imageUrl, '', `width=${width},height=${height},left=${left},top=${top}`);
+// Function to open the image window
+function openImageWindow(imageUrl, images) {
+  imageUrls = images;
+  currentIndex = imageUrls.indexOf(imageUrl);
+  const width = 800; // Set the desired width of the new window
+  const height = 600; // Set the desired height of the new window
 
-    // Focus the new window (optional)
-    if (newWindow) {
-        newWindow.focus();
+  // Calculate the left and top positions for centering the new window
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+
+  // Open a new window with the specified dimensions and URL
+  newWindow = window.open(imageUrl, '', `width=${width},height=${height},left=${left},top=${top}`);
+
+  // Focus the new window (optional)
+  if (newWindow) {
+    newWindow.focus();
+  }
+
+  // Set up a listener to handle keyboard events for the new window
+  newWindow.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      // Previous image (left arrow key)
+      currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+      const imageUrl = imageUrls[currentIndex];
+      newWindow.location.href = imageUrl;
+    } else if (event.key === "ArrowRight") {
+      // Next image (right arrow key)
+      currentIndex = (currentIndex + 1) % imageUrls.length;
+      const imageUrl = imageUrls[currentIndex];
+      newWindow.location.href = imageUrl;
     }
+  });
 }
