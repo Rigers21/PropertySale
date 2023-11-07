6be3b7bf894447192d1e4b4c7415288ceb10a20f
Rigers Collaku <rigers.collaku@icloud.com>
2023-11-07 12:18:36 +0100

    New animations.js3

---
+++ static/scripts/animation3.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.about-us', {delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.services', {delay: 300, origin: 'right'});
